ca53fbae1ae55b6aa4257a823f12584d6b2e4890
Sergey S. Betke <sergey.s.betke@yandex.ru>
2012-10-03 13:55:15 +0400

    Первая версия в GIT
    Signed-off-by: Sergey S. Betke <sergey.s.betke@yandex.ru>

---
+++ test.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module `
+    (join-path `
+        -path ( ( [System.IO.FileInfo] ( $myinvocation.mycommand.path ) ).directory ) `
+        -childPath 'ITG.Translit' `
+    ) `
+    -force `
+;
+
+'Бетке','Сергей','Сергеевич' | convertTo-Translit
