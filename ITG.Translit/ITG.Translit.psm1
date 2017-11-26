f58aaf2fc6bcf8ce7ac70a7bd9d0ebadc53c0dec
Sergey S. Betke <sergey.s.betke@yandex.ru>
2017-11-26 17:30:43 +0300

    * заменены пробелы на табуляцию

--- ITG.Translit/ITG.Translit.psm1
+++ ITG.Translit/ITG.Translit.psm1
@@ -1,131 +1,124 @@
-﻿set-variable `
-    -name GOST_R_52535_A1 `
-    -option constant `
-    -value `
-    @{
-        'А'='A';
-        'Б'='B';
-        'В'='V';
-        'Г'='G';
-        'Д'='D';
-        'Е'='E';
-        'Ё'='E';
-        'Ж'='ZH';
-        'З'='Z';
-        'И'='I';
-        'Й'='I';
-        'К'='K';
-        'Л'='L';
-        'М'='M';
-        'Н'='N';
-        'О'='O';
-        'П'='P';
-        'Р'='R';
-        'С'='S';
-        'Т'='T';
-        'У'='U';
-        'Ф'='F';
-        'Х'='KH';
-        'Ц'='TC';
-        'Ч'='CH';
-        'Ш'='SH';
-        'Щ'='SHCH';
-        'Ь'='';
-        'Ы'='Y';
-        'Ъ'='';
-        'Э'='E';
-        'Ю'='IU';
-        'Я'='IA';
-    } `
+﻿Set-Variable `
+	-Name GOST_R_52535_A1 `
+	-Option constant `
+	-Value @{
+		'А'='A';
+		'Б'='B';
+		'В'='V';
+		'Г'='G';
+		'Д'='D';
+		'Е'='E';
+		'Ё'='E';
+		'Ж'='ZH';
+		'З'='Z';
+		'И'='I';
+		'Й'='I';
+		'К'='K';
+		'Л'='L';
+		'М'='M';
+		'Н'='N';
+		'О'='O';
+		'П'='P';
+		'Р'='R';
+		'С'='S';
+		'Т'='T';
+		'У'='U';
+		'Ф'='F';
+		'Х'='KH';
+		'Ц'='TC';
+		'Ч'='CH';
+		'Ш'='SH';
+		'Щ'='SHCH';
+		'Ь'='';
+		'Ы'='Y';
+		'Ъ'='';
+		'Э'='E';
+		'Ю'='IU';
+		'Я'='IA';
+	} `
 ;
 
 function ConvertTo-Translit {
 	<#
 		.Synopsis
-		    Выполняет транслитерацию исходной строки в соответствии с выбранными правилами транслитерации.
+			Выполняет транслитерацию исходной строки в соответствии с выбранными правилами транслитерации.
 		.Description
-		    Данная функция выполняет транслитерацию исходной строки в соответствии с выбранными правилами транслитерации.
-		.Parameter String
-		    Исходная строка, к которой будут применены правила транслитерации.
-		.Parameter TranslitRules
-			Таблица транслитерации.
+			Данная функция выполняет транслитерацию исходной строки в соответствии с выбранными правилами транслитерации.
 		.Example
-			Создание группы сервисов:
+			Транслитерация массива строк:
 			"Бетке","Сергей","Сергеевич" | ConvertTo-Translit
 	#>
-	
-    
-    param (
+
+	param (
+		# Исходная строка, к которой будут применены правила транслитерации.
 		[Parameter(
 			Mandatory=$true,
 			Position=0,
-			ValueFromPipeline=$true,
-			HelpMessage='Исходная строка, к которой будут применены правила транслитерации.'
+			ValueFromPipeline=$true
 		)]
-        [string]$String,
+		[Alias('SourceString')]
+		[Alias('String')]
+		[String]$InputObject
+	,
+		# Таблица транслитерации.
 		[Parameter(
 			Mandatory=$false,
-			Position=1,
-			ValueFromPipeline=$false,
-			HelpMessage='Таблица транслитерации.'
+			ValueFromPipeline=$false
 		)]
-        $TranslitRules = $GOST_R_52535_A1
+		$TranslitRules = $GOST_R_52535_A1
 	)
 	process {
-        ( `
-            $String -split '' `
-            | % {
-                if ( $TranslitRules.ContainsKey( $_ ) ) {
-                    $TraslitStr = $TranslitRules[$_];
-                    if ( [System.Char]::IsUpper( $_ ) ) {
-                        $TraslitStr.ToUpper();
-                    } else {
-                        $TraslitStr.ToLower();
-                    };
-                } else {
-                    $_;
-                };
-            } `
-        ) -join '';
+		( `
+			$InputObject -split '' `
+			| % {
+				if ( $TranslitRules.ContainsKey( $_ ) ) {
+					$TraslitStr = $TranslitRules[$_];
+					if ( [System.Char]::IsUpper( $_ ) ) {
+						$TraslitStr.ToUpper();
+					} else {
+						$TraslitStr.ToLower();
+					};
+				} else {
+					$_;
+				};
+			} `
+		) -join '';
 	}
-}  
+}
 
 function ConvertFrom-TranslitRules {
 	<#
 		.Synopsis
-		    Конвертация таблицы транслитерации (да и не только) в массив объектов с целью дальнейшей сериализации.
-		.Parameter InputObject
-		    Таблица транслитерации.
+			Конвертация таблицы транслитерации (да и не только) в массив объектов с целью дальнейшей сериализации.
 		.Example
-            $GOST_R_52535_A1 | ConvertFrom-TranslitRules | ConvertTo-HTML -Fragment
+			$GOST_R_52535_A1 | ConvertFrom-TranslitRules | ConvertTo-HTML -Fragment
 	#>
-	
-    
-    param (
+
+	param (
+		# Таблица транслитерации.
 		[Parameter(
 			Mandatory=$true,
 			Position=0,
-			ValueFromPipeline=$true,
-			HelpMessage='Таблица транслитерации.'
+			ValueFromPipeline=$true
 		)]
-        $InputObject
+		$InputObject
 	)
 
 	process {
-        $InputObject.keys `
-        | Select-Object -Property @{
-                Name='Source';
-                Expression={ $_; }
-            }, @{
-                Name='Translit'
-                Expression={ $InputObject[$_]; }
-            } `
-        | Sort-Object 'Source'
-        ;
+		$InputObject.keys `
+		| Select-Object -Property @{
+			Name='Source';
+			Expression={ $_; }
+		}, @{
+			Name='Translit'
+			Expression={ $InputObject[$_]; }
+		} `
+		| Sort-Object 'Source' `
+		;
 	}
-}  
+}
 
 Export-ModuleMember `
-    ConvertTo-Translit `
-    , ConvertFrom-TranslitRules `
+	ConvertTo-Translit `
+	, ConvertFrom-TranslitRules `
 ;
