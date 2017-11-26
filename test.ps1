416ba5c13810f995b0ab5a8cb6b46b8668b956e4
Sergey S. Betke <sergey.s.betke@yandex.ru>
2017-11-26 22:17:25 +0300

    ***

--- test.ps1
+++
@@ -1,9 +0,0 @@
-﻿Import-Module `
-    (join-path `
-        -path ( ( [System.IO.FileInfo] ( $myinvocation.mycommand.path ) ).directory ) `
-        -childPath 'ITG.Translit' `
-    ) `
-    -force `
-;
-
-'Бетке','Сергей','Сергеевич' | convertTo-Translit
