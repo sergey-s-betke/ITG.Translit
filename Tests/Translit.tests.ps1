f64087cb202f83459764e2bb4c4551dacf2dcea2
Sergey S. Betke <sergey.s.betke@yandex.ru>
2017-11-26 17:42:57 +0300

    * добавлено ещё несколько тестов

--- Tests/Translit.tests.ps1
+++ Tests/Translit.tests.ps1
@@ -7,19 +7,20 @@
 
 $ModulePath = ( Join-Path -Path ( Join-Path -Path $RootPath -ChildPath 'ITG.Translit' ) -ChildPath 'ITG.Translit.psm1' );
 
-Import-Module $ModulePath -Force;
+# Import-Module $ModulePath -Force;
 
-# Describe 'Модуль' {
-	# It 'Должен успешно загружаться' {
-		# Import-Module (Join-Path -Path $RootPath -ChildPath 'ITG.Translit.psm1') -Force `
-		# | Should be 'test';
-	# }
-# }
+Describe 'Модуль' {
+	It 'Должен успешно загружаться' {
+		{ Import-Module -FullyQualifiedName $ModulePath -Force -ErrorAction 'Stop' } `
+		| Should Not Throw;
+	}
+}
 
 Describe 'Транслитерация простой строки' {
-	Context 'В случае, когда исходная строка передаётся через конвейер' {
+	Context 'В случае, когда входные данные передаются через конвейер' {
 		It 'Должна вернуть результат транслитерации' {
-			'тест' | ConvertTo-Translit `
+			'тест' `
+			| ConvertTo-Translit `
 			| Should be 'test'
 		}
 	}
@@ -27,6 +28,16 @@ Describe 'Транслитерация простой строки' {
 		It 'Должна вернуть результат транслитерации' {
 			ConvertTo-Translit -SourceString 'тест' `
 			| Should be 'test'
+		}
+	}
+}
+
+Describe 'Транслитерация массива строк' {
+	Context 'В случае, когда входные данные передаются через конвейер' {
+		It 'Должна вернуть массив строк после транслитерации' {
+			'Бетке', 'Сергей', 'Сергеевич' `
+			| ConvertTo-Translit `
+			| Should be 'Betke', 'Sergei', 'Sergeevich'
 		}
 	}
 }
