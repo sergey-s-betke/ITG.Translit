a35714dccba3d9a740f6ac11558073df84289a68
sergey.s.betke <sergey.s.betke@yandex.ru>
2017-11-28 21:49:58 +0300

    * изменён вывод информации об ошибках при тестировании (использован `Write-Error`) + добавлена генерация .md файлов справки и их обновление

--- psakefile.ps1
+++ psakefile.ps1
@@ -4,49 +4,95 @@
 
 param()
 
-# $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop;
-
-Install-Module -Name Psake -SkipPublisherCheck -Scope CurrentUser -ErrorAction Stop -Verbose;
-
-Import-Module Psake -ErrorAction Stop;
+@(
+	@{ ModuleName = 'Psake'; ModuleVersion = '4.7.0' }
+) | Foreach-Object {
+	if ( -not ( Get-Module -FullyQualifiedName $_ -ListAvailable ) ) {
+		Install-Module -Name $_.ModuleName -MinimumVersion $_.ModuleVersion -SkipPublisherCheck -Scope CurrentUser -Force -AllowClobber -ErrorAction Stop;
+	};
+	Import-Module -FullyQualifiedName $_ -ErrorAction Stop -Verbose:$false;
+};
 
 Properties {
-	$SourcesPath = Join-Path -Path $PSScriptRoot -ChildPath 'ITG.Translit';
+	$ModuleName = 'ITG.Translit';
+
+	$SourcesPath = Join-Path -Path $PSScriptRoot -ChildPath $ModuleName;
+	$ModulePath = Join-Path -Path $SourcesPath -ChildPath "$($ModuleName).psm1";
+
 	$TestsPath = Join-Path -Path $PSScriptRoot -ChildPath 'Tests';
 	$TestResultsDirPath = Join-Path -Path $TestsPath -ChildPath 'TestsResults';
 	$TestResultsPath = Join-Path -Path $TestResultsDirPath -ChildPath 'TestsResults.xml';
+
 	$CodeQualityTestsPath = Join-Path -Path $PSScriptRoot -ChildPath 'CodeQualityTests';
 	$CodeQualityTestResultsDirPath = Join-Path -Path $CodeQualityTestsPath -ChildPath 'TestsResults';
 	$ScriptAnalyzerResultsPath = Join-Path -Path $CodeQualityTestResultsDirPath -ChildPath 'ScriptAnalyzerResults.xml';
+
+	$DocsPath = Join-Path -Path $PSScriptRoot -ChildPath 'docs';
+	$MdDocsPath = Join-Path -Path $DocsPath -ChildPath 'markdown';
+
 #	$ArtifactPath = "$Env:BUILD_ARTIFACTSTAGINGDIRECTORY"
 #	$ModuleArtifactPath = "$ArtifactPath\Modules"
+
 	$RequiredModules = @(
+		@{ ModuleName = 'Psake'; ModuleVersion = '4.7.0' }
 		@{ ModuleName = 'Pester'; ModuleVersion = '4.1.0' }
 		@{ ModuleName = 'Coveralls'; ModuleVersion = '1.0.25' }
 		@{ ModuleName = 'PSScriptAnalyzer'; ModuleVersion = '1.16.1' }
+		@{ ModuleName = 'PlatyPS'; ModuleVersion = '0.8.3' }
 	);
 }
 
 Task Default -Depends UnitTests
 
 Task InstallModules {
+	Write-Information 'Install modules...';
+
 	If ( -Not ( Get-PackageProvider -Name NuGet -ListAvailable ) ) {
-		Install-PackageProvider -Name NuGet -Scope CurrentUser -ErrorAction Stop -Force | Out-Null;
-		Import-PackageProvider -Name NuGet -ErrorAction Stop -Force | Out-Null;
+		Install-PackageProvider `
+			-Name NuGet `
+			-Scope CurrentUser `
+			-ErrorAction Stop `
+			-Force `
+			-Verbose:$VerbosePreference `
+		| Out-Null;
+		Import-PackageProvider `
+			-Name NuGet `
+			-ErrorAction Stop `
+			-Force `
+			-Verbose:$VerbosePreference `
+		| Out-Null;
 	};
-	Set-PSRepository -Name PSGallery -InstallationPolicy Trusted;
+	if ( -not ( Get-PSRepository -Name PSGallery ).Trusted ) {
+		Set-PSRepository `
+			-Name PSGallery `
+			-InstallationPolicy Trusted `
+			-Verbose:$VerbosePreference `
+		;
+	};
 	$RequiredModules `
 	| Foreach-Object {
 		if ( -not ( Get-Module -FullyQualifiedName $_ -ListAvailable ) ) {
-			Install-Module -Name $_.ModuleName -RequiredVersion $_.ModuleVersion -SkipPublisherCheck -Scope CurrentUser -Force -AllowClobber -ErrorAction Stop -Verbose;
+			Install-Module `
+				-Name $_.ModuleName `
+				-MinimumVersion $_.ModuleVersion `
+				-SkipPublisherCheck `
+				-Scope CurrentUser `
+				-Force `
+				-AllowClobber `
+				-ErrorAction Stop `
+				-Verbose:$VerbosePreference `
+			;
 		};
-		Import-Module -FullyQualifiedName $_;
+		Import-Module `
+			-FullyQualifiedName $_ `
+			-ErrorAction Stop `
+			-Verbose:$false `
+		;
 	};
 }
 
 Task ScriptAnalysis -Depends InstallModules {
-	# Run Script Analyzer
-	'Starting static analysis...'
+	Write-Information 'Starting static analysis...';
 
 	If ( -Not ( Test-Path $CodeQualityTestResultsDirPath ) ) {
 		New-Item `
@@ -54,6 +100,8 @@ Task ScriptAnalysis -Depends InstallModules {
 			-Name ( Split-Path -Path $CodeQualityTestResultsDirPath -Leaf ) `
 			-ItemType Directory `
 			-Force `
+			-ErrorAction Stop `
+			-Verbose:$VerbosePreference `
 		| Out-Null `
 		;
 	};
@@ -66,6 +114,7 @@ Task ScriptAnalysis -Depends InstallModules {
 	;
 
 	if ( $env:APPVEYOR ) {
+		Write-Verbose 'updloading PSScriptAnalizer results to AppVeyor console...';
 		( New-Object System.Net.WebClient ).UploadFile(
 			"https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)",
 			( Resolve-Path $ScriptAnalyzerResultsPath )
@@ -73,25 +122,16 @@ Task ScriptAnalysis -Depends InstallModules {
 	};
 
 	if ( $PesterResults.FailedCount ) {
-		$errorID = 'AcceptanceTestFailure';
-		$errorCategory = [System.Management.Automation.ErrorCategory]::LimitsExceeded;
-		$errorMessage = "Test Failed: $($PesterResults.FailedCount) tests failed out of $($PesterResults.TotalCount) total test.";
-		$exception = New-Object `
-			-TypeName System.SystemException `
-			-ArgumentList $errorMessage `
+		Write-Error `
+			-ErrorId 'AcceptanceTestFailure' `
+			-Message "Test Failed: $($PesterResults.FailedCount) tests failed out of $($PesterResults.TotalCount) total test." `
+			-Category ( [System.Management.Automation.ErrorCategory]::LimitsExceeded ) `
 		;
-		$errorRecord = New-Object `
-			-TypeName System.Management.Automation.ErrorRecord `
-			-ArgumentList $exception, $errorID, $errorCategory, $null `
-		;
-		Write-Output "##vso[task.logissue type=warning]$errorMessage";
-		Throw $errorRecord;
 	}
 }
 
 Task UnitTests -Depends InstallModules {
-	# Run Unit Tests with Code Coverage
-	'Starting unit tests...'
+	Write-Information 'Starting unit tests...';
 
 	If ( -Not ( Test-Path $TestResultsDirPath ) ) {
 		New-Item `
@@ -99,6 +139,8 @@ Task UnitTests -Depends InstallModules {
 			-Name ( Split-Path -Path $TestResultsDirPath -Leaf ) `
 			-ItemType Directory `
 			-Force `
+			-ErrorAction Stop `
+			-Verbose:$VerbosePreference `
 		| Out-Null `
 		;
 	};
@@ -112,6 +154,7 @@ Task UnitTests -Depends InstallModules {
 	;
 
 	if ( $env:APPVEYOR ) {
+		Write-Verbose 'updloading Pester unit tests results to AppVeyor console...';
 		( New-Object System.Net.WebClient ).UploadFile(
 			"https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)",
 			( Resolve-Path $TestResultsPath )
@@ -126,35 +169,75 @@ Task UnitTests -Depends InstallModules {
 			-PesterResults $PesterResults `
 			-CoverallsApiToken $env:COVERALLS_REPO_TOKEN `
 			-BranchName $env:APPVEYOR_REPO_BRANCH `
+			-Verbose:$VerbosePreference `
 		;
-		Publish-Coverage -Coverage $coverage -Verbose;
+		Publish-Coverage `
+            -Coverage $coverage `
+			-Verbose:$VerbosePreference `
+		;
 	};
 
 	if ( $PesterResults.FailedCount ) {
-		$errorID = 'UnitTestFailure';
-		$errorCategory = [System.Management.Automation.ErrorCategory]::LimitsExceeded;
-		$errorMessage = "Test Failed: $($PesterResults.FailedCount) tests failed out of $($PesterResults.TotalCount) total test.";
-		$exception = New-Object `
-			-TypeName System.SystemException `
-			-ArgumentList $errorMessage `
+		Write-Error `
+			-ErrorId 'UnitTestFailure' `
+			-Message "Test Failed: $($PesterResults.FailedCount) tests failed out of $($PesterResults.TotalCount) total test." `
+			-Category ( [System.Management.Automation.ErrorCategory]::LimitsExceeded ) `
 		;
-		$errorRecord = New-Object `
-			-TypeName System.Management.Automation.ErrorRecord `
-			-ArgumentList $exception, $errorID, $errorCategory, $null `
+	};
+}
+
+Task CreateMarkdownHelp -Depends InstallModules {
+	Write-Information 'Create markdown help files...';
+
+	If ( -Not ( Test-Path $MdDocsPath ) ) {
+		New-Item `
+			-Path ( Split-Path -Path $MdDocsPath -Parent ) `
+			-Name ( Split-Path -Path $MdDocsPath -Leaf ) `
+			-ItemType Directory `
+			-Force `
+			-ErrorAction Stop `
+			-Verbose:$VerbosePreference `
+		| Out-Null `
 		;
-		Write-Output "##vso[task.logissue type=error]$errorMessage";
-		Throw $errorRecord;
-	}
+	};
+
+	Import-Module `
+		-Name $ModulePath `
+		-ErrorAction Stop `
+		-Verbose:$VerbosePreference `
+	;
+	New-MarkdownHelp `
+		-Module $ModuleName `
+		-WithModulePage `
+		-OutputFolder $MdDocsPath `
+		-Locale 'ru-RU' `
+		-Force `
+		-Verbose:$VerbosePreference `
+	| Out-Null;
+}
+
+Task UpdateMarkdownHelp -Depends InstallModules {
+	Write-Information 'Update markdown help files...';
+
+	Import-Module `
+		-Name $ModulePath `
+		-ErrorAction Stop `
+		-Verbose:$VerbosePreference `
+	;
+	Update-MarkdownHelp `
+		-Path $MdDocsPath `
+		-Verbose:$VerbosePreference `
+	| Out-Null;
 }
 
 Task Clean {
-	'Starting Cleaning enviroment...'
+	Write-Information 'Starting Cleaning enviroment...';
 
 	If ( Test-Path $TestResultsDirPath ) {
-		Remove-Item $TestResultsDirPath -Recurse -Force -ErrorAction Continue;
+		Remove-Item $TestResultsDirPath -Recurse -Force -ErrorAction Continue -Verbose:$VerbosePreference;
 	};
 	If ( Test-Path $CodeQualityTestResultsDirPath ) {
-		Remove-Item $CodeQualityTestResultsDirPath -Recurse -Force -ErrorAction Continue;
+		Remove-Item $CodeQualityTestResultsDirPath -Recurse -Force -ErrorAction Continue -Verbose:$VerbosePreference;
 	};
 
 	$Error.Clear();
